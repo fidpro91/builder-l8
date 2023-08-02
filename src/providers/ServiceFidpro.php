e6319fd066830a7b2f119bdc23083d6497489e73
fidpro91 <ufi.alfi@gmail.com>
2023-08-02 14:02:24 +0700

    updated

--- src/providers/ServiceFidpro.php
+++ src/providers/ServiceFidpro.php
@@ -13,16 +13,28 @@ class ServiceFidpro extends ServiceProvider
 
     public function boot(Filesystem $filesystem)
     {
-        // Lakukan konfigurasi atau bootstrapping di sini
-        /* $this->publishes([
-            __DIR__.'/path/ke/file/konfigurasi.php' => app_path('nama_file_konfigurasi.php'),
-        ], 'config'); */
+        // Perform any package specific bootstrapping here
+        // Example:
+        // $this->loadMigrationsFrom(__DIR__.'/path/to/migrations');
+        // $this->loadViewsFrom(__DIR__.'/path/to/views', 'example');
+        
+        // Publish the commands folder to app/Console
         $this->publishes([
-            __DIR__.'/../commands' => app_path('Console'),
-        ], 'fidpro-commands');
+            __DIR__.'/../commands'  => app_path('Console/Commands'),
+            __DIR__.'/../builder'   => resource_path('stubs'),
+            __DIR__.'/../default-assets'   => public_path('assets'),
+            __DIR__.'/../Helpers'   => app_path('Helpers'),
+            __DIR__.'/../layouts'   => resource_path('views/templates'),
+        ], 'fidpro-l8');
 
-        // Copy the entire folder recursively
-        $filesystem->copyDirectory(__DIR__.'/../commands', app_path('Console'));
+        // Copy the files inside the commands folder to app/Console/commands
+        $filesystem->copy(__DIR__.'/../core/Controller.php', app_path('Http/Controllers/Controller.php'), true);
+        $filesystem->copyDirectory(__DIR__.'/../commands', app_path('Console/Commands'));
+        $filesystem->copyDirectory(__DIR__.'/../builder', resource_path('stubs'));
+        $filesystem->copyDirectory(__DIR__.'/../layouts', resource_path('views/templates'));
+
+        // Merge configuration if needed
+        // $this->mergeConfigFrom(__DIR__.'/config/example.php', 'example');
     }
 }
 
