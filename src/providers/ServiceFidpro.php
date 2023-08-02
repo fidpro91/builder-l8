c076405bf02662114e9efac345399a9d01bbb402
fidpro91 <ufi.alfi@gmail.com>
2023-08-02 14:06:13 +0700

    updated

--- src/providers/ServiceFidpro.php
+++ src/providers/ServiceFidpro.php
@@ -20,7 +20,7 @@ class ServiceFidpro extends ServiceProvider
         
         // Publish the commands folder to app/Console
         $this->publishes([
-            __DIR__.'/../commands'  => app_path('Console/Commands'),
+            __DIR__.'/../Commands'  => app_path('Console/Commands'),
             __DIR__.'/../builder'   => resource_path('stubs'),
             __DIR__.'/../default-assets'   => public_path('assets'),
             __DIR__.'/../Helpers'   => app_path('Helpers'),
@@ -29,7 +29,7 @@ class ServiceFidpro extends ServiceProvider
 
         // Copy the files inside the commands folder to app/Console/commands
         $filesystem->copy(__DIR__.'/../core/Controller.php', app_path('Http/Controllers/Controller.php'), true);
-        $filesystem->copyDirectory(__DIR__.'/../commands', app_path('Console/Commands'));
+        $filesystem->copyDirectory(__DIR__.'/../Commands', app_path('Console/Commands'));
         $filesystem->copyDirectory(__DIR__.'/../builder', resource_path('stubs'));
         $filesystem->copyDirectory(__DIR__.'/../layouts', resource_path('views/templates'));
 
