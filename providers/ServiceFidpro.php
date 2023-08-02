a82e48dc44e591fddad3dc67d30d48290727688a
fidpro91 <ufi.alfi@gmail.com>
2023-08-02 12:35:27 +0700

    updated

---
+++ providers/ServiceFidpro.php
@@ -0,0 +1,25 @@
+<?php
+namespace fidpro\providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceFidpro extends ServiceProvider
+{
+    public function register()
+    {
+        // Daftarkan service atau binding di sini
+    }
+
+    public function boot()
+    {
+        // Lakukan konfigurasi atau bootstrapping di sini
+        /* $this->publishes([
+            __DIR__.'/path/ke/file/konfigurasi.php' => app_path('nama_file_konfigurasi.php'),
+        ], 'config'); */
+        $this->publishes([
+            __DIR__.'/commands' => app_path('Console'),
+        ], 'Commands');
+    }
+}
+
+?>
