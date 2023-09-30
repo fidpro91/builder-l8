921b79d83bc5a5934f4195b882baa61c6ecdde16
fidpro91 <ufi.alfi@gmail.com>
2023-09-30 08:41:25 +0700

    updated

--- src/Migrations/2023_09_26_004028_create_ms_menu_table.php
+++ src/Migrations/2023_09_26_004028_create_ms_menu_table.php
@@ -13,18 +13,20 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('ms_menu', function (Blueprint $table) {
-            $table->char('menu_code', 20);
-            $table->string('menu_name', 140);
-            $table->text('menu_url')->nullable();
-            $table->integer('menu_parent_id')->nullable()->default(0);
-            $table->boolean('menu_status')->nullable()->default(true);
-            $table->string('menu_icon', 30)->nullable();
-            $table->integer('modul_id')->nullable();
-            $table->increments('menu_id');
-            $table->string('menu_target', 20)->nullable();
-            $table->string('menu_function')->nullable();
-        });
+        if (!Schema::hasTable('ms_menu')) {
+            Schema::create('ms_menu', function (Blueprint $table) {
+                $table->char('menu_code', 20);
+                $table->string('menu_name', 140);
+                $table->text('menu_url')->nullable();
+                $table->integer('menu_parent_id')->nullable()->default(0);
+                $table->boolean('menu_status')->nullable()->default(true);
+                $table->string('menu_icon', 30)->nullable();
+                $table->integer('modul_id')->nullable();
+                $table->increments('menu_id');
+                $table->string('menu_target', 20)->nullable();
+                $table->string('menu_function')->nullable();
+            });
+        }
     }
 
     /**
