b728a97f14db95e64b9160f63644ab5407291837
fidpro91 <ufi.alfi@gmail.com>
2023-10-08 12:02:05 +0700

    updated

--- src/layouts/documentation/sidebar.blade.php
+++ src/layouts/documentation/sidebar.blade.php
@@ -6,15 +6,6 @@ use Illuminate\Support\Facades\Session;
 <div class="left-side-menu">
 
     <div class="slimscroll-menu">
-
-        <!-- User box -->
-        <div class="user-box text-center">
-            <img src="{{asset('assets/themes/image/patient.png')}}" alt="user-img" title="Mat Helme" class="rounded-circle img-thumbnail avatar-lg">
-            <p class="text-muted">
-                
-            </p>
-        </div>
-
         <!--- Sidemenu -->
         <div id="sidebar-menu">
             <ul class="metismenu side-menu">
